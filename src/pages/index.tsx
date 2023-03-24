d8a1441c757de311a1a2e861a0d36f573f7d93b0
ROSSAAI <128677255+ROSSAAI@users.noreply.github.com>
2023-03-24 13:08:03 +0900

    Head with metadatas have been added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,38 @@ import React from "react";
 import Header from "../partials/Header";
 import HeroHome from "../partials/HeroHome";
 import Footer from "../partials/Footer";
+import Head from "next/head";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
-      <Header />
+    <>
+      <Head>
+        <title>ROSSA AI | Dare to create</title>
+        <meta
+          property="og:title"
+          content="ROSSA AI | Dare to create"
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Our Image2Code, transforms any UI design into code for the most popular frameworks."
+        />
+        <meta
+          property="og:description"
+          content="Our Image2Code, transforms any UI design into code for the most popular frameworks."
+        />
+        <meta property="og:image" content="https://rossa.ai/favicon.png" />
+      </Head>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <Header />
 
-      {/*  Page content */}
-      <main className="flex-grow">
-        {/*  Page sections */}
-        <HeroHome />
-      </main>
+        <main className="flex-grow">
+          <HeroHome />
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
           <div className="sm:col-span-12 lg:col-span-3">
-            <Link href="/" className="inline-block" aria-label="ROSSA">
-              <Image src={Favicon} width={64} alt="ROSSA Logotype"/>
+            <Link href="/" className="flex items-center" aria-label="ROSSA">
+              <Image src={Favicon} width={64} alt="ROSSA Logotype" />
+              <p className="text-md text-black ml-4 -mt-0.5">Dare to create</p>
             </Link>
-        
           </div>
 
           {/* <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
